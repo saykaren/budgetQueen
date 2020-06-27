5202722232e69301768ef90f98f0a2ce502ce41d
Karen Garcia <saykaren@gmail.com>
2020-06-26 20:45:54 -0600

    Updates

---
+++ precache-manifest.289db34ac4f261bade82d0b9743c94b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21072e88fba044c8779f8879514c0deb",
+    "url": "/budgetQueen/index.html"
+  },
+  {
+    "revision": "b314915118f6f1d52eee",
+    "url": "/budgetQueen/static/css/main.39ae5f4e.chunk.css"
+  },
+  {
+    "revision": "c39b8975e1e81c9eea1a",
+    "url": "/budgetQueen/static/js/2.bf020ffd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/budgetQueen/static/js/2.bf020ffd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b314915118f6f1d52eee",
+    "url": "/budgetQueen/static/js/main.8662e4c6.chunk.js"
+  },
+  {
+    "revision": "ae177b4e608b5b6c0bb9",
+    "url": "/budgetQueen/static/js/runtime-main.cf86654c.js"
+  }
+]);
