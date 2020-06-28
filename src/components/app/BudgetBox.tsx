a3f9f95b1d186ee9182ff90f3629111d1125b81c
Karen Garcia <saykaren@gmail.com>
2020-06-27 21:48:20 -0600

    Updated SampleData
    Updated SampleData to include actual and estimate items and worked on the BudgetBox render to see how I
    want to show the results.

--- src/components/app/BudgetBox.tsx
+++ src/components/app/BudgetBox.tsx
@@ -10,28 +10,50 @@ const BudgetBox = ({ data }: BudgetBoxProps) => {
   console.log(SampleData);
   return (
     <div className="budgetBox">
-       {SampleData &&
-       SampleData.map((num,index)=>(
-        <div key={index}>
-        <h2 >{num.monthName}</h2>
-    <div className='budget-detail'>${num.begBalance}</div>
-    <div className='budget-detail'>${num.endBalance}</div>
-    </div>
-       ))
-       }
-      <h2>Month </h2>
-      <div className="budget-container">
-        <div className="budget-headers">Budget Item</div>
-        <div className="budget-estimates">Budget Estimate</div>
-        <div className="budget-actual">Budget Actual</div>
-        <div className="budget-eval">Evaluation</div>
-      </div>
-      <div className="budget-container">
-        <div className="budget-headers">Items</div>
-        <div className="budget-estimates">$500</div>
-        <div className="budget-actual">$550</div>
-        <div className="budget-eval">($50)</div>
-      </div>
+      {SampleData &&
+        SampleData.map((num, numIndex) => (
+          <div key={numIndex}>
+            <h2>{num.monthName}</h2>
+            <div className="budget-overall-box">
+              <div className="budget-detail">
+                {" "}
+                Beginning Balance ${num.begBalance}
+              </div>
+              <div className="budget-detail">
+                Ending Balance ${num.endBalance}
+              </div>
+            </div>
+            <div className="budget-income-box">
+              <h2>Income</h2>
+              <section className="estimate-actual-holder">
+                {num.budget.map((budgetNum, indexbudget) => (
+                  <>
+                    <div className="budget-estimate-income" key={indexbudget}>
+                      <h3>Estimates</h3>
+                      {budgetNum.monthContributions.map((mc, mcIndex) => (
+                        <div className="budget-income" key={mcIndex}>
+                          {mc.title} ${mc.amount}
+                        </div>
+                      ))}
+                    </div>
+                  </>
+                ))}
+                {num.actual.map((actualNum, actIndexbudget) => (
+                  <>
+                    <div className="budget-actual-income" key={actIndexbudget}>
+                      <h3>Actual</h3>
+                      {actualNum.monthContributions.map((ac, acIndex) => (
+                        <div className="budget-income" key={acIndex}>
+                          {ac.title} ${ac.amount}
+                        </div>
+                      ))}
+                    </div>
+                  </>
+                ))}
+              </section>
+            </div>
+          </div>
+        ))}
     </div>
   );
 };
--- src/components/app/SampleData.tsx
+++ src/components/app/SampleData.tsx
@@ -1,20 +1,158 @@
 export const TestData = [
-    {
-        monthId: 1,
-        monthName: "January",
-        begBalance: 4000,
-        endBalance: 1000,
-        monthExpenses: [{title: "Bought stuff", amount: 4000, monthIN: 1}],
-        monthContributions: [{title: "Side gig", amount: 1000, monthIN: 1}],
-    },
-    {
-        monthId: 2,
-        monthName: "February",
-        begBalance: 1000,
-        endBalance: 900,
-        monthExpenses: [{title: "Bought Stuff again", amount: 200, monthIN: 1}],
-        monthContributions: [{title: "side gig", amount: 100, monthIN: 1}],
-    }
+  {
+    monthId: 1,
+    monthName: "January",
+    year: 2020,
+    begBalance: 4000,
+    endBalance: 2900,
+    budget: [
+      {
+        monthExpenses: [
+          {
+            housingExpense: [{ title: "Rent", amount: 1800, monthIN: 1 }],
+          },
+          {
+            groceryExpense: [
+              { title: "King Soopers", amount: 500, monthIN: 1 },
+              { title: "Costco", amount: 400, monthIN: 1 },
+            ],
+          },
+          {
+            discretionaryExpense: [
+              { title: "Amazon", amount: 200, monthIN: 1 },
+            ],
+          },
+          {
+            retirementSavings: [{ title: "Roth", amount: 100, monthIN: 1 }],
+          },
+          {
+            collegeSavings: [{ title: "529", amount: 100, monthIN: 1 }],
+          },
+          {
+            otherExpense: [
+              { title: "Extra Mortgage Payment", amount: 100, monthIN: 1 },
+            ],
+          },
+        ],
+        monthContributions: [
+          { title: "Side gig", amount: 100, monthIN: 1 },
+          { title: "Payroll", amount: 2000, monthIN: 1 },
+        ],
+      },
+    ],
+    actual: [
+      {
+        monthExpenses: [
+          {
+            housingExpense: [{ title: "Rent", amount: 1800, monthIN: 1 }],
+          },
+          {
+            groceryExpense: [
+              { title: "King Soopers", amount: 500, monthIN: 1 },
+              { title: "Costco", amount: 400, monthIN: 1 },
+            ],
+          },
+          {
+            discretionaryExpense: [
+              { title: "Amazon", amount: 200, monthIN: 1 },
+            ],
+          },
+          {
+            retirementSavings: [{ title: "Roth", amount: 100, monthIN: 1 }],
+          },
+          {
+            collegeSavings: [{ title: "529", amount: 100, monthIN: 1 }],
+          },
+          {
+            otherExpense: [
+              { title: "Extra Mortgage Payment", amount: 100, monthIN: 1 },
+            ],
+          },
+        ],
+        monthContributions: [
+          { title: "Side gig", amount: 52, monthIN: 1 },
+          { title: "Payroll", amount: 2003, monthIN: 1 },
+        ],
+      },
+    ],
+  },
+  {
+    monthId: 2,
+    monthName: "February",
+    year: 2020,
+    begBalance: 4000,
+    endBalance: 2900,
+    budget: [
+      {
+        monthExpenses: [
+          {
+            housingExpense: [{ title: "Rent", amount: 1800, monthIN: 1 }],
+          },
+          {
+            groceryExpense: [
+              { title: "King Soopers", amount: 500, monthIN: 1 },
+              { title: "Costco", amount: 400, monthIN: 1 },
+            ],
+          },
+          {
+            discretionaryExpense: [
+              { title: "Amazon", amount: 200, monthIN: 1 },
+            ],
+          },
+          {
+            retirementSavings: [{ title: "Roth", amount: 100, monthIN: 1 }],
+          },
+          {
+            collegeSavings: [{ title: "529", amount: 100, monthIN: 1 }],
+          },
+          {
+            otherExpense: [
+              { title: "Extra Mortgage Payment", amount: 100, monthIN: 1 },
+            ],
+          },
+        ],
+        monthContributions: [
+          { title: "Side gig", amount: 100, monthIN: 1 },
+          { title: "Payroll", amount: 2000, monthIN: 1 },
+        ],
+      },
+    ],
+    actual: [
+      {
+        monthExpenses: [
+          {
+            housingExpense: [{ title: "Rent", amount: 1800, monthIN: 1 }],
+          },
+          {
+            groceryExpense: [
+              { title: "King Soopers", amount: 500, monthIN: 1 },
+              { title: "Costco", amount: 400, monthIN: 1 },
+            ],
+          },
+          {
+            discretionaryExpense: [
+              { title: "Amazon", amount: 200, monthIN: 1 },
+            ],
+          },
+          {
+            retirementSavings: [{ title: "Roth", amount: 100, monthIN: 1 }],
+          },
+          {
+            collegeSavings: [{ title: "529", amount: 100, monthIN: 1 }],
+          },
+          {
+            otherExpense: [
+              { title: "Extra Mortgage Payment", amount: 100, monthIN: 1 },
+            ],
+          },
+        ],
+        monthContributions: [
+          { title: "Side gig", amount: 75, monthIN: 1 },
+          { title: "Payroll", amount: 2501, monthIN: 1 },
+        ],
+      },
+    ],
+  },
 ];
 
 export default TestData;
