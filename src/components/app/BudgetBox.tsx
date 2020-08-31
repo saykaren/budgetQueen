e08b98991be58198813fa00e4facebdbf08195e1
Karen Garcia <saykaren@gmail.com>
2020-08-30 21:01:42 -0600

    Started Menu Started the menu section. I want to get the format then I will add the input fields to change the different sections.

--- src/components/app/BudgetBox.tsx
+++ src/components/app/BudgetBox.tsx
@@ -5,24 +5,38 @@ import ComparsionDetails from "./ComparisonDetails";
 
 const BudgetBox = () => {
   const [data, setData] = useState(SampleData);
+  const [BeginBalanceState, setBeginBalanceState] = useState(4000);
 
   console.log(data);
   return (
     <div className="budgetBox">
       {data &&
         data.map((num, numIndex) => (
-          <div key={numIndex}>
+          <div key={numIndex} className="monthDetail">
             <h2>{num.monthName}</h2>
             <div className="budget-overall-box">
-              <div className="budget-detail">
-                {" "}
-                Beginning Balance ${num.begBalance}
-              </div>
-              <div className="budget-detail">
-                Ending Balance ${num.endBalance}
+              <div        className={
+                num.begBalance >= 0
+                  ? 'budget-detail positiveBalance'
+                  : 'budget-detail negativeBalance'
+              }
+              
+              >
+                <b>Beginning Balance ${num.begBalance}</b>
               </div>
             </div>
             <ComparsionDetails title={"Income2"} object={num} />
+            <div className="budget-overall-box">
+              <div 
+               className={
+                num.endBalance >= 0
+                  ? 'budget-detail positiveBalance'
+                  : 'budget-detail negativeBalance'
+              }
+              >
+                Ending Balance ${num.endBalance}
+              </div>
+            </div>
           </div>
         ))}
     </div>
---
+++ src/components/app/Menu.tsx
@@ -0,0 +1,21 @@
+import React, {useState} from 'react';
+
+
+interface MenuProps {
+
+}
+
+const Menu = ({}: MenuProps)=>{
+
+    return(
+        <>
+        <section className='inputSection'>
+            <div className='inputLabel'>
+                Menu
+            </div>
+        </section>
+        </>
+    )
+}
+
+export default Menu;
