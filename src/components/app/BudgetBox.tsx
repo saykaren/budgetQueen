eb48a15f7737aaf3636a244b5c9015a692f24dcc
Karen Garcia <saykaren@gmail.com>
2020-09-23 21:09:09 -0600

    Calculations Component Got my caculations component separated. Learned typescript inline as well as got the props better.

--- src/components/app/BudgetBox.tsx
+++ src/components/app/BudgetBox.tsx
@@ -3,66 +3,13 @@ import TestDataInterface, { SampleDataArrayProps } from "./TestDataInterface";
 import SampleData from "./SampleData";
 import ComparsionDetails from "./ComparisonDetails";
 import Menu from "./Menu";
-import Calculate from './Calculate';
+import Calculate from "./Calculate";
 
 const BudgetBox = () => {
   const [data, setData] = useState(SampleData);
   const [menu, setMenu] = useState(false);
   const [BeginBalanceState, setBeginBalanceState] = useState(4000);
-
-  const calculate = () => {
-    let adjustData = data;
-
-    let totalExpensesArray : number[] = [];
-    let sumExpenses : number = 0;
-
-    let totalIncomeArray: number[] = [];
-    let sumIncome : number = 0;
-  
-    const totalIncomeFunction = () => {
-      adjustData[0].actual[0].monthContributions.map((income) =>
-        totalIncomeArray.push(income.amount)
-      );
  
-    };
-    totalIncomeFunction();
-  
-
-    const totalExpenseFunction = () => {
-      adjustData[0].actual[0].monthExpenses.housingExpense.map((expense) =>
-      totalExpensesArray.push(expense.amount)
-      );
-      adjustData[0].actual[0].monthExpenses.groceryExpense.map((expense) =>
-      totalExpensesArray.push(expense.amount)
-      );
-      adjustData[0].actual[0].monthExpenses.discretionaryExpense.map((expense) =>
-      totalExpensesArray.push(expense.amount)
-      );
-      adjustData[0].actual[0].monthExpenses.retirementSavings.map((expense) =>
-      totalExpensesArray.push(expense.amount)
-      );
-      adjustData[0].actual[0].monthExpenses.collegeSavings.map((expense) =>
-      totalExpensesArray.push(expense.amount)
-      );
-      adjustData[0].actual[0].monthExpenses.otherExpense.map((expense) =>
-      totalExpensesArray.push(expense.amount)
-      );
-  
-    };
-    totalExpenseFunction();
-   
-
-    sumExpenses = totalExpensesArray.reduce((acc,num)=> acc+ num, 0)
-    sumIncome = totalIncomeArray.reduce((acc,num)=> acc+ num, 0)
- 
-
-    let monthNetAmount = sumIncome - sumExpenses;
-    console.table({monthNetAmount, sumExpenses, sumIncome, totalIncomeArray, totalExpensesArray})
-  };
-
-
-
-  console.log(data);
   return (
     <div className="budgetBox">
       {menu && (
@@ -79,10 +26,11 @@ const BudgetBox = () => {
           &raquo;
         </div>
       )}
-      <div onClick={() => calculate()} id="refresh">
-        Refresh
-      </div>
+
+      <Calculate data={data} />
+   
       <h2 className="appTitle">Budget Queen</h2>
+      {/* <Calculate data={data}/> */}
       {data &&
         data.map((num, numIndex) => (
           <div key={numIndex} className="monthDetail">
--- src/components/app/Calculate.tsx
+++ src/components/app/Calculate.tsx
@@ -1,62 +1,83 @@
 import React from "react";
-import { TestDataProps } from "./TestDataInterface";
+import SampleDataArrayProps, { TestDataProps } from "./TestDataInterface";
 
 interface CalculateProps {
-    data: TestDataProps;
-} 
-
-
-const Calculate = ({data}: CalculateProps)=>{
-
-    // const calculate = () => {
-    //     let adjustData: TestDataProps = data;
-    
-    //     let totalExpensesArray : number[] = [];
-    //     let sumExpenses : number = 0;
-    
-    //     let totalIncomeArray: number[] = [];
-    //     let sumIncome : number = 0;
-      
-    //     const totalIncomeFunction = () => {
-    //       adjustData[0].actual[0].monthContributions.map((income) =>
-    //         totalIncomeArray.push(income.amount)
-    //       );
-     
-    //     };
-    //     totalIncomeFunction();
-      
-    
-    //     const totalExpenseFunction = () => {
-    //       adjustData[0].actual[0].monthExpenses.housingExpense.map((expense) =>
-    //       totalExpensesArray.push(expense.amount)
-    //       );
-    //       adjustData[0].actual[0].monthExpenses.groceryExpense.map((expense) =>
-    //       totalExpensesArray.push(expense.amount)
-    //       );
-    //       adjustData[0].actual[0].monthExpenses.discretionaryExpense.map((expense) =>
-    //       totalExpensesArray.push(expense.amount)
-    //       );
-    //       adjustData[0].actual[0].monthExpenses.retirementSavings.map((expense) =>
-    //       totalExpensesArray.push(expense.amount)
-    //       );
-    //       adjustData[0].actual[0].monthExpenses.collegeSavings.map((expense) =>
-    //       totalExpensesArray.push(expense.amount)
-    //       );
-    //       adjustData[0].actual[0].monthExpenses.otherExpense.map((expense) =>
-    //       totalExpensesArray.push(expense.amount)
-    //       );
-      
-    //     };
-    //     totalExpenseFunction();
-       
-    
-    //     sumExpenses = totalExpensesArray.reduce((acc,num)=> acc+ num, 0)
-    //     sumIncome = totalIncomeArray.reduce((acc,num)=> acc+ num, 0)
-     
-    
-    //     let monthNetAmount = sumIncome - sumExpenses;
-    //     console.table({monthNetAmount, sumExpenses, sumIncome, totalIncomeArray, totalExpensesArray})
-    //   };
+  data: Array<TestDataProps>;
 }
+
+const Calculate = ({ data }: CalculateProps) => {
+  let newData: Array<TestDataProps> = data;
+
+  const calculateMe = () => {
+    newData.map((month, indexMonth) => {
+      console.log({ indexMonth });
+
+      let totalExpensesArray: number[] = [];
+      let sumExpenses: number = 0;
+
+      let totalIncomeArray: number[] = [];
+      let sumIncome: number = 0;
+
+      const totalIncomeFunction = () => {
+        newData[indexMonth].actual[0].monthContributions.map((income) =>
+          totalIncomeArray.push(income.amount)
+        );
+      };
+      totalIncomeFunction();
+
+      const totalExpenseFunction = () => {
+        newData[
+          indexMonth
+        ].actual[0].monthExpenses.housingExpense.map((expense) =>
+          totalExpensesArray.push(expense.amount)
+        );
+        newData[
+          indexMonth
+        ].actual[0].monthExpenses.groceryExpense.map((expense) =>
+          totalExpensesArray.push(expense.amount)
+        );
+        newData[
+          indexMonth
+        ].actual[0].monthExpenses.discretionaryExpense.map((expense) =>
+          totalExpensesArray.push(expense.amount)
+        );
+        newData[
+          indexMonth
+        ].actual[0].monthExpenses.retirementSavings.map((expense) =>
+          totalExpensesArray.push(expense.amount)
+        );
+        newData[
+          indexMonth
+        ].actual[0].monthExpenses.collegeSavings.map((expense) =>
+          totalExpensesArray.push(expense.amount)
+        );
+        newData[
+          indexMonth
+        ].actual[0].monthExpenses.otherExpense.map((expense) =>
+          totalExpensesArray.push(expense.amount)
+        );
+      };
+      totalExpenseFunction();
+
+      sumExpenses = totalExpensesArray.reduce((acc, num) => acc + num, 0);
+      sumIncome = totalIncomeArray.reduce((acc, num) => acc + num, 0);
+
+      let monthNetAmount = sumIncome - sumExpenses;
+      console.table({
+        monthNetAmount,
+        sumExpenses,
+        sumIncome,
+        totalIncomeArray,
+        totalExpensesArray,
+      });
+    });
+  };
+
+  return (
+    <div onClick={() => calculateMe()} id="refresh">
+      Refresh Me
+    </div>
+  );
+};
 
 export default Calculate;
